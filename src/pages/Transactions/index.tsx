a285367d159e594955472419ee0d648030994a59
feliperbdantas <feliperbdantas@outlook.com>
2024-05-21 20:29:44 -0300

    feat: Connecting to the API - Transactions Page - Integrating Json Server

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -5,29 +7,29 @@ import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 interface Transactions {
-  description: string;
-  value: string;
-  type: string;
-  date: string;
+  id: number;
+  title: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
 }
 
-const transactions: Transactions[] = [
-  {
-    description: "Desenvolvimento de site",
-    value: "R$ 12.000,00",
-    type: "Venda",
-    date: "13/04/2022"
-  },
-  {
-    description: "Hamburger",
-    value: "- R$ 59,00",
-    type: "Alimentação",
-    date: "10/04/2022"
-  }
-]
-
-
 export function Transactions () {
+  const [transactions, setTransactions] = useState<Transactions[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+
+    const data: Transactions[] = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -39,14 +41,14 @@ export function Transactions () {
 
         <TransactionsTable>
           <tbody>
-            { transactions.map((item) => (
-              <tr key={item.description}>
-                <td width="50%">{item.description}</td>
+            { transactions &&transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.title}</td>
                 <td>
-                  <PriceHighLight variant={item.type === "Venda" ? "income" : "outcome"}>{ item.value }</PriceHighLight>
+                  <PriceHighLight variant={transaction.type}>{ transaction.price }</PriceHighLight>
                 </td>
-                <td>{ item.type }</td>
-                <td>{ item.date }</td>
+                <td>{ transaction.category }</td>
+                <td>{ transaction.createdAt }</td>
               </tr>
             )) }
           </tbody>
