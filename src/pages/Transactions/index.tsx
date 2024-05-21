d4f62523953fe824366b83f6f61254b3cd9c6e72
feliperbdantas <feliperbdantas@outlook.com>
2024-05-21 00:15:04 -0300

    feat: Visual Structure - Modal Component: New Transaction

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,29 @@ import { SearchForm } from "./components/SearchForm";
 
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transactions {
+  description: string;
+  value: string;
+  type: string;
+  date: string;
+}
+
+const transactions: Transactions[] = [
+  {
+    description: "Desenvolvimento de site",
+    value: "R$ 12.000,00",
+    type: "Venda",
+    date: "13/04/2022"
+  },
+  {
+    description: "Hamburger",
+    value: "- R$ 59,00",
+    type: "Alimentação",
+    date: "10/04/2022"
+  }
+]
+
+
 export function Transactions () {
   return (
     <div>
@@ -16,22 +39,16 @@ export function Transactions () {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            { transactions.map((item) => (
+              <tr key={item.description}>
+                <td width="50%">{item.description}</td>
+                <td>
+                  <PriceHighLight variant={item.type === "Venda" ? "income" : "outcome"}>{ item.value }</PriceHighLight>
+                </td>
+                <td>{ item.type }</td>
+                <td>{ item.date }</td>
+              </tr>
+            )) }
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
